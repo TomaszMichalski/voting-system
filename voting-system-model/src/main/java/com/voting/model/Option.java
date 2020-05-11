3676cbc53bfefd00f59e08665788efb884ac62cc
TomaszMichalski <tomasz.michalski19@gmail.com>
2020-05-11 15:55:35 +0200

    Project skeleton

---
+++ voting-system-model/src/main/java/com/voting/model/Option.java
@@ -0,0 +1,22 @@
+package com.voting.model;
+
+public class Option {
+
+    private String name;
+
+    public Option() {
+
+    }
+
+    public Option(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ voting-system-service/src/main/java/com/voting/service/VotingSystemServiceApplication.java
@@ -0,0 +1,12 @@
+package com.voting.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSystemServiceApplication.class, args);
+	}
+}
