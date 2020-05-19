d621695253aab40f38b681a551a4a146cdb10414
Andrzej97 <szafand@gmail.com>
2020-05-19 19:41:55 +0200

    More refactoring in MessageBroker

--- voting-system-message-broker/src/main/java/com/voting/MessageBroker.java
+++ voting-system-message-broker/src/main/java/com/voting/MessageBroker.java
@@ -5,9 +5,9 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.ThreadLocalRandom;
@@ -15,53 +15,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MessageBroker {
 
-	public ConnectionData connectionData;
-	public ExchangeData exchangeData;
-	private String typePrezydenckie = "WYBORY PREZYDENCKIE 2020";
+	private ExchangeData exchangeData;
 	private Channel channel;
     
-    public MessageBroker(ConnectionData connectionData, ExchangeData exchangeData) {
-    	this.connectionData = connectionData;
+    public MessageBroker(ConnectionData connectionData, ExchangeData exchangeData) throws IOException, TimeoutException {
     	this.exchangeData = exchangeData;
-    	channel = null;
-        try {
-            channel = createChannel();
-            //TODO: change to log
-            System.out.println(" [ResultsSender] Connection and channel created.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-            return;
-        }
- 
+    	this.channel = createChannel(connectionData);
     }
 
-    public void loopUserInput()
-    {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        AMQP.BasicProperties properties = new AMQP.BasicProperties();
-        HashMap <String, Object> headersMap = new HashMap<String, Object>();
-        headersMap.put("Candidate 1", 0);
-        headersMap.put("Candidate 2", 0);
-        headersMap.put("Candidate 3", 0);
-        while (true) {
-        	headersMap.put("Candidate 1", (int)headersMap.get("Candidate 1") + ThreadLocalRandom.current().nextInt(0, 100));
-            headersMap.put("Candidate 2", (int)headersMap.get("Candidate 2") + ThreadLocalRandom.current().nextInt(0, 100));
-            headersMap.put("Candidate 3", (int)headersMap.get("Candidate 3") + ThreadLocalRandom.current().nextInt(0, 100));
-            properties = properties.builder().headers(headersMap).type(typePrezydenckie).build();
-            System.out.println("[Press enter to send next results] > ");
-            try {
-                String messageContent = bufferedReader.readLine();
-                channel.basicPublish(exchangeData.name, "", properties, messageContent.getBytes());
-            } catch (IOException e) {
-            }
-        }
-    }
-
-    protected Channel createChannel() throws IOException, TimeoutException {
+    private Channel createChannel(ConnectionData connectionData) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(connectionData.ip);
         factory.setUsername(connectionData.user);
@@ -73,5 +35,19 @@ public class MessageBroker {
         channel.exchangeDeclare(exchangeData.name, exchangeData.type);
 
         return channel;
+    }
+    
+    public void loopUserInput(String type) throws IOException {
+        AMQP.BasicProperties properties = new AMQP.BasicProperties();
+        HashMap <String, Object> headersMap = new HashMap<String, Object>();
+        headersMap.put("Candidate 1", 0);
+        headersMap.put("Candidate 2", 0);
+        headersMap.put("Candidate 3", 0);
+    	headersMap.put("Candidate 1", (int)headersMap.get("Candidate 1") + ThreadLocalRandom.current().nextInt(0, 100));
+        headersMap.put("Candidate 2", (int)headersMap.get("Candidate 2") + ThreadLocalRandom.current().nextInt(0, 100));
+        headersMap.put("Candidate 3", (int)headersMap.get("Candidate 3") + ThreadLocalRandom.current().nextInt(0, 100));
+        properties = properties.builder().headers(headersMap).type(type).build();
+        channel.basicPublish(exchangeData.name, "", properties, null);
+
     }
 }
--- voting-system-message-broker/src/main/java/com/voting/ResultsBroker.java
+++ voting-system-message-broker/src/main/java/com/voting/ResultsBroker.java
@@ -1,5 +1,11 @@
 package com.voting;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeoutException;
+import java.io.BufferedReader;
+
 public class ResultsBroker {
     public final static String messageBrokerIp = "127.0.0.1";
     public final static String messageBrokerUser = "bunny";
@@ -12,8 +18,26 @@ public class ResultsBroker {
 		System.out.println("Hello from results broker");
     	ConnectionData connectionData = new ConnectionData(messageBrokerIp, messageBrokerUser, messageBrokerPwd, messageBrokerVirtualHost);
     	ExchangeData exchangeData = new ExchangeData(exchangeName, "fanout");
-		MessageBroker resultsSender = new MessageBroker(connectionData, exchangeData);
-		resultsSender.loopUserInput();
+    	
+        try {
+        	MessageBroker messageBroker = new MessageBroker(connectionData, exchangeData);
+            //TODO: change to log
+            System.out.println(" [ResultsSender] Connection and channel created.");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                System.out.println("[Press enter to send next results] > ");
+                try {
+                    String messageContent = bufferedReader.readLine();
+                    messageBroker.loopUserInput(typePrezydenckie);
+                } catch (IOException e) {
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+            return;
+        }
 	}
-	
 }
