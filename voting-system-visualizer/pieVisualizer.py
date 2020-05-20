6d677f4f679aa570644764dec852aa117061aa54
Andrzej97 <szafand@gmail.com>
2020-05-20 20:17:02 +0200

    Python pieVisualizer: subplots prototype added

--- voting-system-visualizer/pieVisualizer.py
+++ voting-system-visualizer/pieVisualizer.py
@@ -7,8 +7,12 @@ class PieVisualizer:
     def __init__(self, votings_names):
         self.votings_names = votings_names
         self.votes = {}
-        self.fig, self.ax = plt.subplots()
-        self.ax.axis('equal')
+        self.fig, self.ax = plt.subplots(1, len(self.votings_names))
+        self.fig.suptitle('VOTINGS RESULTS')
+        if 1 == len(self.votings_names):
+            self.ax = [self.ax]
+        for i in range(0, len(self.votings_names)):
+            self.ax[i].set_title(votings_names[i])
         self.plot('')
 
     def update_votes(self, title, votes):
@@ -17,9 +21,11 @@ class PieVisualizer:
 
     def plot(self, title):
         labels, results = dict_to_labels_and_values(self.votes)
-        plt.cla()
-        self.ax.pie(results, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-        plt.title(title)
+        for i in range(0, len(self.votings_names)):
+            self.ax[i].cla()
+            self.ax[i].axis('equal')
+            self.ax[i].set_title(self.votings_names[i])
+            self.ax[i].pie(results, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
         plt.pause(0.1)
 
 
