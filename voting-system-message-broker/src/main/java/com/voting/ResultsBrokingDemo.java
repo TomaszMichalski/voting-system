33027c6b94afafab088c41159e5695a46d014983
Andrzej97 <szafand@gmail.com>
2020-05-21 00:29:11 +0200

    Java messageBroker demo updated. Python visualizer improvements.

--- voting-system-message-broker/src/main/java/com/voting/ResultsBrokingDemo.java
+++ voting-system-message-broker/src/main/java/com/voting/ResultsBrokingDemo.java
@@ -19,26 +19,52 @@ public class ResultsBrokingDemo {
             return;
         }
 
-        HashMap <String, Object> votes = new HashMap<String, Object>();
-        votes.put("Candidate 1", 0);
-        votes.put("Candidate 2", 0);
-        votes.put("Candidate 3", 0);
-        votes.put("Candidate 4", 0);
-        votes.put("Candidate 5", 0);
+        HashMap <String, Object> votes2 = new HashMap<String, Object>();
+        votes2.put("Candidate 1", 0);
+        votes2.put("Candidate 2", 0);
+        
+        HashMap <String, Object> votes5 = new HashMap<String, Object>();
+        votes5.put("Name1", 0);
+        votes5.put("Name2", 0);
+        votes5.put("Name3", 0);
+        votes5.put("Name4", 0);
+        votes5.put("Name5", 0);
+        
+        HashMap <String, Object> votes8 = new HashMap<String, Object>();
+        votes8.put("A", 0);
+        votes8.put("B", 0);
+        votes8.put("C", 0);
+        votes8.put("D", 0);
+        votes8.put("E", 0);
+        votes8.put("F", 0);
+        votes8.put("G", 0);
+        votes8.put("H", 0);
+        
         while (true) {
         		int pauseDuration = ThreadLocalRandom.current().nextInt(300, 1500);
         		Thread.sleep(pauseDuration);
-
-            	votes.put("Candidate 2", (int)votes.get("Candidate 2") + ThreadLocalRandom.current().nextInt(0, 38));
-                votes.put("Candidate 4", (int)votes.get("Candidate 4") + ThreadLocalRandom.current().nextInt(0, 34));
-                votes.put("Candidate 1", (int)votes.get("Candidate 1") + ThreadLocalRandom.current().nextInt(0, 14));
-                votes.put("Candidate 3", (int)votes.get("Candidate 3") + ThreadLocalRandom.current().nextInt(0, 8));
-                votes.put("Candidate 5", (int)votes.get("Candidate 5") + ThreadLocalRandom.current().nextInt(0, 6));
                 
-                if(pauseDuration % 2 == 0) {
-                	resultsBroker.publishResults("WYBORY PREZYDENCKIE 2020", votes);
+                if(pauseDuration % 3 == 0) {
+                	votes2.put("Candidate 1", (int)votes2.get("Candidate 1") + ThreadLocalRandom.current().nextInt(0, 50));
+                    votes2.put("Candidate 2", (int)votes2.get("Candidate 2") + ThreadLocalRandom.current().nextInt(0, 51));
+                	resultsBroker.publishResults("votings2", votes2);
+                } else if(pauseDuration % 3 == 1){
+                	votes5.put("Name2", (int)votes5.get("Name2") + ThreadLocalRandom.current().nextInt(0, 38));
+                    votes5.put("Name4", (int)votes5.get("Name4") + ThreadLocalRandom.current().nextInt(0, 34));
+                    votes5.put("Name1", (int)votes5.get("Name1") + ThreadLocalRandom.current().nextInt(0, 14));
+                    votes5.put("Name3", (int)votes5.get("Name3") + ThreadLocalRandom.current().nextInt(0, 8));
+                    votes5.put("Name5", (int)votes5.get("Name5") + ThreadLocalRandom.current().nextInt(0, 6));
+                	resultsBroker.publishResults("votings5", votes5);
                 } else {
-                	resultsBroker.publishResults("WYBORY PRZYKLADOWE", votes);
+                	votes8.put("A", (int)votes8.get("A") + ThreadLocalRandom.current().nextInt(0, 2));
+                    votes8.put("B", (int)votes8.get("B") + ThreadLocalRandom.current().nextInt(0, 8));
+                    votes8.put("C", (int)votes8.get("C") + ThreadLocalRandom.current().nextInt(0, 16));
+                    votes8.put("D", (int)votes8.get("D") + ThreadLocalRandom.current().nextInt(0, 9));
+                    votes8.put("E", (int)votes8.get("E") + ThreadLocalRandom.current().nextInt(0, 6));
+                    votes8.put("F", (int)votes8.get("F") + ThreadLocalRandom.current().nextInt(0, 30));
+                    votes8.put("G", (int)votes8.get("G") + ThreadLocalRandom.current().nextInt(0, 3));
+                    votes8.put("H", (int)votes8.get("H") + ThreadLocalRandom.current().nextInt(0, 26));
+                	resultsBroker.publishResults("votings8", votes8);
                 }
         }
 
--- voting-system-visualizer/pieVisualizer.py
+++ voting-system-visualizer/pieVisualizer.py
@@ -7,26 +7,32 @@ class PieVisualizer:
     def __init__(self, votings_names):
         self.votings_names = votings_names
         self.votes = {}
-        self.fig, self.ax = plt.subplots(1, len(self.votings_names))
-        self.fig.suptitle('VOTINGS RESULTS')
-        if 1 == len(self.votings_names):
-            self.ax = [self.ax]
-        for i in range(0, len(self.votings_names)):
-            self.ax[i].set_title(votings_names[i])
-        self.plot('')
+        for i in range(0, len(votings_names)):
+            self.votes[votings_names[i]] = {}
+        self.initialize_plots(len(votings_names))
+        self.plot_draft()
 
     def update_votes(self, title, votes):
-        self.votes = votes
+        self.votes[title] = votes
         self.plot(title)
 
     def plot(self, title):
-        labels, results = dict_to_labels_and_values(self.votes)
         for i in range(0, len(self.votings_names)):
             self.ax[i].cla()
             self.ax[i].axis('equal')
             self.ax[i].set_title(self.votings_names[i])
+            labels, results = dict_to_labels_and_values(self.votes[self.votings_names[i]])
             self.ax[i].pie(results, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
         plt.pause(0.1)
+
+    def plot_draft(self):
+        self.plot('')
+
+    def initialize_plots(self, num):
+        self.fig, self.ax = plt.subplots(1, num)
+        self.fig.suptitle('VOTINGS RESULTS')
+        if 1 == num:
+            self.ax = [self.ax]
 
 
 def dict_to_labels_and_values(d):
