114937715b5283e96648042d9a013b5781a060d3
TomaszMichalski <tomasz.michalski19@gmail.com>
2020-06-01 09:17:41 +0200

    Vote event logging

--- voting-system-service/src/main/java/com/voting/service/VoteService.java
+++ voting-system-service/src/main/java/com/voting/service/VoteService.java
@@ -12,13 +12,18 @@ import com.voting.service.exception.ResourceNotFoundException;
 import com.voting.service.payload.VoteRequest;
 import com.voting.service.security.UserPrincipal;
 import com.voting.service.validator.VoteValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VoteService {
+    private static final Logger logger = LoggerFactory.getLogger(VoteService.class);
 
     @Autowired
     private VoteRepository voteRepository;
@@ -48,6 +53,8 @@ public class VoteService {
                 .map(optionId -> toVotingOption(voting, optionId))
                 .map(option -> toVote(voter, option, voting, voteTime))
                 .forEach(voteRepository::save);
+
+        logger.info("Vote in voting " + voting.getId() + ", options " + toOptionLog(voteRequest.getOptionIds()));
     }
 
     private Option toVotingOption(Voting voting, Long optionId) {
@@ -60,5 +67,11 @@ public class VoteService {
 
     private Vote toVote(Voter voter, Option selectedOption, Voting voting, LocalDateTime voteTime) {
         return new Vote(voter, selectedOption, voting, voteTime);
+    }
+
+    private String toOptionLog(List<Long> optionIds) {
+        return optionIds.stream()
+                .map(optionId -> String.valueOf(optionId.longValue()))
+                .collect(Collectors.joining(","));
     }
 }
