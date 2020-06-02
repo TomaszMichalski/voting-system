c8076414aa25db8d8b29ad7024ddea77cbf0eea2
Andrzej97 <szafand@gmail.com>
2020-06-02 15:31:28 +0200

    Added ConfigReader for messageBroker

---
+++ voting-system-message-broker/src/main/java/com/voting/ConfigReader.java
@@ -0,0 +1,85 @@
+package com.voting;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ConfigReader {
+	private String filename;
+	private String messageBrokerIp;
+    private String messageBrokerUser;
+    private String messageBrokerPwd;
+    private String messageBrokerVirtualHost;
+    private String exchangeName;
+	
+    public ConfigReader(String filename) throws FileNotFoundException{
+    	this.filename = filename;
+    	this.loadConfig();
+    }
+
+    public String getMessageBrokerIp() {
+    	return this.messageBrokerIp;
+    }
+    
+    public String getMessageBrokerUser() {
+    	return this.messageBrokerUser;
+    }
+    
+    public String getMessageBrokerPwd() {
+    	return this.messageBrokerPwd;
+    }
+    
+    public String getMessageBrokerVirtualHost() {
+    	return this.messageBrokerVirtualHost;
+    }
+    
+    public String getExchangeName() {
+    	return this.exchangeName;
+    }
+    private void loadConfig() throws FileNotFoundException {
+    	File file = new File(this.filename);
+    	
+    	messageBrokerIp = loadMessageBrokerIp(file);
+    	messageBrokerUser = loadMessageBrokerUser(file);
+    	messageBrokerPwd = loadMessageBrokerPwd(file);
+    	messageBrokerVirtualHost = loadMessageBrokerVirtualHost(file);
+    	exchangeName = loadExchangeName(file);
+    }
+    
+    private String loadMessageBrokerIp(File file) throws FileNotFoundException {
+    	return loadConfValue(file, "messageBrokerId:");
+    }
+    
+    private String loadMessageBrokerUser(File file) throws FileNotFoundException {
+    	return loadConfValue(file, "messageBrokerUser:");
+    }
+    
+    private String loadMessageBrokerPwd(File file) throws FileNotFoundException {
+    	return loadConfValue(file, "messageBrokerPwd:");
+    }
+    
+    private String loadMessageBrokerVirtualHost(File file) throws FileNotFoundException {
+    	return loadConfValue(file, "messageBrokerVirtualHost:");
+    }
+    
+    private String loadExchangeName(File file) throws FileNotFoundException {
+    	return loadConfValue(file, "exchangeName:");
+    }
+    
+    private String loadConfValue(File file, String beggining) throws FileNotFoundException {
+    	Scanner reader = new Scanner(file);
+    	while (reader.hasNextLine()) {
+            String data = reader.nextLine();
+            if(data.startsWith(beggining)) {
+                reader.close();
+            	return getValueAfterColon(data);
+            }
+    	}
+        reader.close();
+    	throw new IllegalArgumentException(beggining + " not found in given config file");
+    }
+    
+    private String getValueAfterColon(String line) {
+    	return line.substring(line.lastIndexOf(":") + 1).replaceAll("\\s+","");
+    }
+}
